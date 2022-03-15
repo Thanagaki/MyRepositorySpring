900c066aeecbe44c5acae10dcd53513d90938966
Thanagaki <100522683+Thanagaki@users.noreply.github.com>
2022-03-15 17:18:49 +0300

    Add files via upload

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,13 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal {
+
+    public String toString () {
+        return "I am a dog";
+    }
+
+
+}
